2fc9233dcc3954057af2a5df0e905e3907ed016d
munetsi <zunguziramunetsi@gmail.com>
2017-07-07 16:45:44 +0200

    posts in descending order

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.order("created_at ASC").limit(10).all
+    @posts = Post.order("created_at DESC").limit(10).all
   end
 
   def about
