985e459e7cfc52d33abb46fe3d7b4e8ec27d1ada
munetsi <zunguziramunetsi@gmail.com>
2017-07-09 20:43:25 +0200

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 10, maximum: 50}
+  validates :content, presence: true, length: {minimum: 50, maximum: 700}
 end
